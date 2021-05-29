881290920e483bd94385f55fbfe11b40a9ced1a5
Yena-Yun <cheryleduart@gmail.com>
2021-05-29 18:08:51 +0900

    cookie 함수들 만들고 로그인에 반영

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from '../components/Header';
-import { Grid, Text, Button } from '../elements';
+import { Grid, Text, Button, Input } from '../elements';
+import { setCookie } from '../shared/Cookie';
+
 
 const Login = (props) => {
+	// useState로 state 선언
+	const [id, setId] = React.useState('');
+	const [pwd, setPwd] = React.useState('');
+
+	// event가 들어오면 state를 바꾸는 함수 2개 선언
+	const changeId = (e) => {
+		setId(e.target.value);
+	}
+	const changePwd = (e) => {
+		setPwd(e.target.value);
+	}
+
+	// 로그인 버튼 눌렀을 때 실행 (쿠키에 저장)
+	// (import한 setCookie 함수 활용)
+	const login = () => {
+		// key, value, 만료일
+		setCookie("user_id", id, 3);
+		setCookie("user_pwd", pwd, 3);
+	}
+
 	return (
 		<LoginBlock>
 			<Header />
@@ -11,13 +33,15 @@ const Login = (props) => {
 				<Text size="32" margin="66 0 16" bold>로그인</Text>
 				<InputGrid>
 					<Text>아이디</Text>
-					<Inputbox type="text" id="username" placeholder="아이디를 입력하세요." />
+					<Inputbox type="text" id="username" value={id} onChange={changeId} placeholder="아이디를 입력하세요." />
 				</InputGrid>
 				<InputGrid>
 					<Text>비밀번호</Text>
-					<Inputbox type="password" id="password" placeholder="비밀번호를 입력하세요." />
+					<Inputbox type="password" id="password" value={pwd} onChange={changePwd}placeholder="비밀번호를 입력하세요." />
 				</InputGrid>
-					<Button width="343px" height="50px" bg="black">
+				<Button __click={() => {
+					login();
+				}} width="343px" height="50px" bg="black">
 						<Text color="white">로그인하기</Text>
 					</Button>
 			</Grid>
---
+++ src/shared/Cookie.js
@@ -0,0 +1,42 @@
+// key값을 기준으로 쿠키에 저장된 value를 가져오는 함수
+const getCookie = (name) => {
+
+	//쿠키 값을 가져온다.
+	//document.cookie = '; MY_COOKIE=here;'
+	let value = "; " + document.cookie;
+
+	//키 값(name)을 기준으로 파싱한다.
+	//(split: ()안의 내용을 기준으로 나누어서 새로운 배열로 반환)
+	let parts = value.split("; " + name + "=");
+	//["; MY_COOKIE=", "here;"]
+
+	//value를 반환한다.
+	if (parts.length == 2) {
+		//parts 배열의 마지막 값(pop)을 ';'로 나눈 후(split) 
+		//첫 번째 값(key)을 반환(shift) (=> value만 반환됨)
+		//(push(): 배열의 맨 뒤에 값 추가, unshift(): 배열의 맨 앞에 값 추가)
+		return parts.pop().split(";").shift();
+	}
+};
+
+// 쿠키에 key와 value, 만료일을 저장하는 함수 (exp = 5 : 5일 후 만료)
+const setCookie = (name, value, exp = 5) => {
+	//날짜 객체 생성
+	let date = new Date();
+
+	//Date 객체에 만료일 날짜 지정
+	//getTime(): 1970년 1월 1일 0시 0분 0초부터 현재까지의 시간 (= 현재 시간)
+	//현재 시간 + 만료될 날짜까지의 일수 (둘 다 밀리초로 환산)
+	date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
+	
+	//쿠키에 key, value, 만료일 저장
+	//(만료일에는 date 객체를 문자열로 바꾸어 저장)
+	document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/`;
+};
+
+// 쿠키 삭제 => 만료일을 예전일로 설정
+const deleteCookie = (name) => {
+	document.cookie = name + '=; expires=Thu, 01 Jan 1999 00:00:10 GMT;';
+};
+
+export { getCookie, setCookie, deleteCookie };
